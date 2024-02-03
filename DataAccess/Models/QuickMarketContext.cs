09686430ab64bdc9783123c630efcaef8cd48e44
Nguyễn Hoàng Kiên <hoangkien08012002@gmail.com>
2024-02-03 21:15:42 +0700

    update appsetting

--- DataAccess/Models/QuickMarketContext.cs
+++ DataAccess/Models/QuickMarketContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 namespace DataAccess.Models
 {
@@ -34,8 +35,10 @@ namespace DataAccess.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server =localhost; database = QuickMarket;uid=sa;pwd=sa;TrustServerCertificate=true");
+                var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+                string ConnectionStr = config.GetConnectionString("DB");
+
+                optionsBuilder.UseSqlServer(ConnectionStr);
             }
         }
 
