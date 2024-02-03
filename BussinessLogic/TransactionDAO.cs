95fbcef242b3ecb44638f6ef76d6a562f6a29a2f
tungb12ok <99113840+tungb12ok@users.noreply.github.com>
2024-02-03 22:08:24 +0700

    Update Repository and Singleton Transaction

---
+++ BussinessLogic/TransactionDAO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Models;
+namespace BussinessLogic
+{
+    public class TransactionDAO
+    {
+        private QuickMarketContext context;
+        private static TransactionDAO instance = null;
+        private readonly static Object instanceLock = new Object();
+        public TransactionDAO(QuickMarketContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public TransactionDAO()
+        {
+        }
+
+        public static TransactionDAO Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new TransactionDAO();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+    }
+}
