2cbd1ab51c7265712a08b829c6961985741919c7
Nguyễn Hoàng Kiên <hoangkien08012002@gmail.com>
2024-02-03 21:06:48 +0700

    Update CRUD for User

---
+++ WEB/Pages/User/Create.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using DataAccess.Models;
+
+namespace WEB.Pages.User
+{
+    public class CreateModel : PageModel
+    {
+        private readonly DataAccess.Models.QuickMarketContext _context;
+
+        public CreateModel(DataAccess.Models.QuickMarketContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["CategoryId"] = new SelectList(_context.ProductCategories, "CategoryId", "CategoryName");
+        ViewData["StatusId"] = new SelectList(_context.Statuses, "StatusId", "StatusName");
+            return Page();
+        }
+
+        [BindProperty]
+        public Product Product { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Products == null || Product == null)
+            {
+                return Page();
+            }
+
+            _context.Products.Add(Product);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
