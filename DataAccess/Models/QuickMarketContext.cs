dd99332f19c4bc0cd180552af793656dadd3be1e
tungb12ok <99113840+tungb12ok@users.noreply.github.com>
2024-03-19 00:11:28 +0700

    Update

--- DataAccess/Models/QuickMarketContext.cs
+++ DataAccess/Models/QuickMarketContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 namespace DataAccess.Models
 {
