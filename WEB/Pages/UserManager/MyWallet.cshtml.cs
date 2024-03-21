d26669a9ed873025c0cc55078e956fdedd5b0e48
tungb12ok <99113840+tungb12ok@users.noreply.github.com>
2024-03-22 00:01:04 +0700

    Update My Wallet

--- WEB/Pages/UserManager/MyWallet.cshtml.cs
+++ WEB/Pages/UserManager/MyWallet.cshtml.cs
@@ -1,3 +1,5 @@
+using DataAccess.Models;
+using DocumentFormat.OpenXml.Spreadsheet;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,29 @@ namespace WEB.Pages.UserManager
 {
     public class MyWalletModel : PageModel
     {
-        public void OnGet()
+        private readonly QuickMarketContext _context;
+
+        public MyWalletModel(QuickMarketContext context)
         {
+            _context = context;
+        }
+        public List<FinancialTransaction> ListFT {  get; set; }
+        public IActionResult OnGet()
+        {
+            User u = Extenstions.SessionExtensions.Get<User>(HttpContext.Session, "User");
+            if (u == null)
+            {
+                return Redirect("/SignIn");
+            }
+            ListFT = _context.FinancialTransactions.Where(x => x.UserId == u.UserId).ToList();
+            decimal total = _context.FinancialTransactions.Where(x => x.UserId == u.UserId).Sum(x => x.Amount);
+            var lastTime = _context.FinancialTransactions
+                .Where(ft => ft.UserId == u.UserId)
+                .OrderByDescending(ft => ft.TransactionDate)
+                .FirstOrDefault().TransactionDate;
+            ViewData["summary"] = new { total, lastTime };
+            return Page();
+
         }
     }
 }
